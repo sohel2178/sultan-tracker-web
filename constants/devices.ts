02b713a92c72eaa73a5e55f88efad5250f48d4fe
sohel2178 <sohel.ahmed2178@gmail.com>
2025-02-15 16:44:25 +0600

    correction on creating dummy devices

--- constants/devices.ts
+++ constants/devices.ts
@@ -3,34 +3,34 @@ const generateDevices = (count: number): Device[] => {
 
   for (let i = 1; i <= count; i++) {
     devices.push({
-      _id: `67a8969684031a0cb52d5a${i.toString().padStart(2, "0")}`,
-      id: `54547845${i.toString().padStart(2, "0")}`,
-      deviceSimNumber: `01944537${(7000 + i).toString()}`,
-      registrationNumber: `DH_MET-KA-12-45${(70 + i).toString()}`,
-      vehicleModel: ["Axios", "Toyota", "Suzuki", "Honda", "BMW"][i % 5],
-      code: "",
-      centerNumber: `01835645${(6200 + i).toString()}`,
-      vehicleType: ["Car", "Bike", "Truck", "Bus", "CNG"][
+      _id: `67a8969684031a0cb52d5a${i.toString().padStart(2, '0')}`,
+      id: `54547845${i.toString().padStart(2, '0')}`,
+      device_sim_number: `01944537${(7000 + i).toString()}`,
+      registration_number: `DH_MET-KA-12-45${(70 + i).toString()}`,
+      vehicle_model: ['Axios', 'Toyota', 'Suzuki', 'Honda', 'BMW'][i % 5],
+      code: '',
+      center_number: `01835645${(6200 + i).toString()}`,
+      vehicle_type: ['Car', 'Bike', 'Truck', 'Bus', 'CNG'][
         i % 5
-      ] as Device["vehicleType"],
+      ] as Device['vehicle_type'],
       mileage: Math.floor(Math.random() * 50),
-      congestionConsumption: Math.floor(Math.random() * 5) + 1,
-      serviceCharge: Math.floor(Math.random() * 500) + 100,
-      speedLimit: Math.floor(Math.random() * 60) + 60,
-      maxTemp: Math.floor(Math.random() * 50) + 30,
-      minTemp: Math.floor(Math.random() * 15) + 10,
-      deviceModel: {
-        _id: `67a4b36fddc6071b07c7db${i.toString().padStart(2, "0")}`,
-        name: `QB-${i.toString().padStart(3, "0")}`,
+      congestion_consumption: Math.floor(Math.random() * 5) + 1,
+      service_charge: Math.floor(Math.random() * 500) + 100,
+      speed_limit: Math.floor(Math.random() * 60) + 60,
+      max_temp: Math.floor(Math.random() * 50) + 30,
+      min_temp: Math.floor(Math.random() * 15) + 10,
+      device_model: {
+        _id: `67a4b36fddc6071b07c7db${i.toString().padStart(2, '0')}`,
+        name: `QB-${i.toString().padStart(3, '0')}`,
       } as Model,
       reference: {
-        _id: `67a4dc111ee67c3374e5ad${i.toString().padStart(2, "0")}`,
+        _id: `67a4dc111ee67c3374e5ad${i.toString().padStart(2, '0')}`,
         name: [
-          "Abid Sobhan",
-          "Rahim Uddin",
-          "Karim Ali",
-          "Nasir Khan",
-          "Sohan Mia",
+          'Abid Sobhan',
+          'Rahim Uddin',
+          'Karim Ali',
+          'Nasir Khan',
+          'Sohan Mia',
         ][i % 5],
       } as Reference,
     });
